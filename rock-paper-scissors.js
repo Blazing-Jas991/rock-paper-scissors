120847c89ed1e5481a24ee8d88bddcdc20338a75
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2025-09-25 07:51:20 -0700

    fix player and computer input and response

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,7 +10,7 @@ function getComputerChoice() {
     }  else {
         return "scissors";
     }
-  }
+}
 
 function getHumanChoice() {
   let yourPick = prompt("Please enter your option:");
@@ -32,14 +32,21 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
-    if (humanChoice === "rock" || "paper" || "scissors") {
-      console.log(humanChoice);
-      console.log(getComputerChoice());
-      alert(computerChoice);
-    } 
-
-}
-    
+    if (humanChoice === computerChoice) {
+      alert("There is a Tie");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      alert("Human Wins");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      alert("Human Wins");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      alert("Human Wins");
+    } else {
+      alert("Computer Wins");
+    }
+    console.log(humanChoice);
+    console.log(computerChoice);
+  
+}    
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
