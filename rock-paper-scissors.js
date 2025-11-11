7b26d65d420aaec1ac3d1cb2be314e478d4557fc
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2025-11-11 08:28:48 -0800

    Add buttons and player selection logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,85 +1,60 @@
+alert("We Are About To Play Rock, Paper, Scissors!");
+
 function getComputerChoice() {
-  const randomChoice = Math.ceil(Math.random() * 3);
+    const randomChoice = Math.ceil(Math.random() * 3);
 
-  if (randomChoice === 1) {
-    return "rock";
+    if (randomChoice === 1) {
+       return "rock";
 
-  }else if (randomChoice === 2) {
-    return "paper";
+    }  else if (randomChoice === 2) {
+       return "paper";
 
-  }else {
-    return "scissors";
-  };
-};
-
-let yourChoice = "";
-yourChoice.textContent = "";
+    }  else {
+       return "scissors";
+    }
+}
 
 function getHumanChoice() {
-  let inputArea = document.createElement("input");
+  let yourChoice = prompt("Please enter your option:");
+  yourChoice = yourChoice.toLowerCase();
 
-  let userInput = prompt("Please enter your option:");
-  userInput.toLowerCase();
-  if (userInput === null) { 
-    return;
+  if (yourChoice === "rock") {
+    return "rock";
 
-  } else if (userInput === "rock") {
-    yourChoice.textContent = "Rock";
-    console.log("rock");
-
-  } else if (userInput === "paper") {
-    yourChoice.textContent = "Paper";
-    console.log("paper");
-
-  } else if (userInput === "scissors") {
-    yourChoice.textContent = "Scissors";
-    console.log("scissors");
+  } else if (yourChoice === "paper") {
+    return "paper";
 
   } else {
-    return '';
-  };
-};
+    return "scissors";
+  }
+}
 
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice.toLowerCase();
-    
+    humanChoice = humanChoice.toLowerCase();
     if (humanChoice === computerChoice) {
       console.log("There is a Tie, Try Again");
-
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-      yourChoice.textContent = "Rock";
       console.log("You Win, Rock Crushes Scissors!");
       humanScore++;
-
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-      yourChoice.textContent = "Scissors";
       console.log("You Win, Scissors Cuts Paper");
       humanScore++;
-
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-      yourChoice.textContent = "Paper";
       console.log("You Win, Paper covers Rock");
       humanScore++;
-
     } else if (computerChoice === "rock" && humanChoice === "scissors") {
-      yourChoice.textContent = "Scissors";
       console.log("Computer Wins, Rock Crushes Scissors");
       computerScore++;
-
     } else if (computerChoice === "scissors" && humanChoice === "paper") {
-      yourChoice.textContent = "Paper";
-      console.log("Computer Wins, Scissors Cuts Paper");
+      console.log("Compuer Wins, Scissors Cuts Paper");
       computerScore++;
-
     } else if (computerChoice === "paper" && humanChoice === "rock") {
-      yourChoice.textContent = "Rock";
       console.log("Computer Wins, Paper Covers Rock");
       computerScore++;
-
     } else {
       console.log("Try Again, that's not a valid option");
     }
@@ -89,21 +64,21 @@ function playRound(humanChoice, computerChoice) {
     console.log('Your Score: ' + humanScore);
     console.log("Computer's Score: " + computerScore);
 
-};   
+}   
 
 function playGame() {
-  const humanChoice = getHumanChoice();
-  const computerChoice = getComputerChoice();
-  playRound(humanChoice, computerChoice);
-  const diff = computerScore-humanScore;
-  
-  if (computerScore > humanScore) {
-    alert("Computer Won");
-  } else {
-    alert("You Won");
+  for (let i = 0; i < 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    const diff = computerScore-humanScore;
   }
+    if (computerScore > humanScore) {
+      alert("Computer Won");
+    } else {
+      alert("You Won");
+    }
     
-};
+}
 
 playGame();
-
