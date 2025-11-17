397fca3517e130cc999cf9d8b4c9461fcb8af44c
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2025-11-17 00:28:44 -0800

    Finish work

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,84 +1,150 @@
-alert("We Are About To Play Rock, Paper, Scissors!");
+const rockButton = document.querySelector("#button1");
+const paperButton = document.querySelector("#button2");
+const scissorsButton = document.querySelector("#button3");
+const container = document.querySelector("#container");
+const containerTwo = document.querySelector(".container-two")
+let span = document.querySelector("#input-text");
+let cSpan = document.querySelector("#computer-text");
+let resultSpan = document.querySelector("#round-winner");
+let yourScore = document.querySelector("#yourScore");
+let comScore = document.querySelector("#comScore");
+let scoreCard = document.querySelector("#scoreCard");
+let humanScore = 0;
+let computerScore = 0;
+let clickCounter = 4;
+let resetButton = document.createElement("button");
 
 function getComputerChoice() {
     const randomChoice = Math.ceil(Math.random() * 3);
-
+    let computerPick = "";
     if (randomChoice === 1) {
-       return "rock";
-
-    }  else if (randomChoice === 2) {
-       return "paper";
-
-    }  else {
-       return "scissors";
-    };
-};
-
-function getHumanChoice() {
-  let yourChoice = prompt("Please enter your option:");
-  yourChoice = yourChoice.toLowerCase();
-
-  if (yourChoice === "rock") {
-    return "rock";
-
-  } else if (yourChoice === "paper") {
-    return "paper";
-
-  } else {
-    return "scissors";
-  };
-};
-
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice === computerChoice) {
-      console.log("There is a Tie, Try Again");
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-      console.log("You Win, Rock Crushes Scissors!");
-      humanScore++;
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-      console.log("You Win, Scissors Cuts Paper");
-      humanScore++;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-      console.log("You Win, Paper covers Rock");
-      humanScore++;
-    } else if (computerChoice === "rock" && humanChoice === "scissors") {
-      console.log("Computer Wins, Rock Crushes Scissors");
-      computerScore++;
-    } else if (computerChoice === "scissors" && humanChoice === "paper") {
-      console.log("Compuer Wins, Scissors Cuts Paper");
-      computerScore++;
-    } else if (computerChoice === "paper" && humanChoice === "rock") {
-      console.log("Computer Wins, Paper Covers Rock");
-      computerScore++;
+        computerPick = "Rock";
+        cSpan.textContent = "Computer Picked: " + computerPick;
+        return computerPick;
+    } else if (randomChoice === 2) {
+        computerPick = "Paper";
+        cSpan.textContent = "Computer Picked: " + computerPick;
+        return computerPick;
     } else {
-      console.log("Try Again, that's not a valid option");
+        computerPick = "Scissors";
+        cSpan.textContent = "Computer Picked: " + computerPick;
+        return computerPick;
     };
-    
-    console.log('You Chose: ' + humanChoice);
-    console.log('Computer Chose: ' + computerChoice);
-    console.log('Your Score: ' + humanScore);
-    console.log("Computer's Score: " + computerScore);
+};
 
-}   
+rockButton.addEventListener("click", chooseRock);
+function chooseRock() {
+    span.textContent = "You picked: Rock";
+    playGame();
+    remainingMoves();
+    comScore.textContent = "Computer Score: " + computerScore;
+    yourScore.textContent = "Your Score: " + humanScore;
+    clickCounter --;
+};
+
+paperButton.addEventListener("click", choosePaper);
+function choosePaper() {
+    span.textContent = "You picked: Paper";
+    playGame();
+    remainingMoves();
+    comScore.textContent = "Computer Score: " + computerScore;
+    yourScore.textContent = "Your Score: " + humanScore;
+    clickCounter --;
+};
+
+scissorsButton.addEventListener("click", chooseScissors);
+function chooseScissors() {
+    span.textContent = "You picked: Scissors";
+    playGame();
+    remainingMoves();
+    comScore.textContent = "Computer Score: " + computerScore;
+    yourScore.textContent = "Your Score: " + humanScore;
+    clickCounter --;
+};
 
 function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-    const diff = computerScore-humanScore;
-  };
-    if (computerScore > humanScore) {
-      alert("Computer Won");
+    let computerChoice = getComputerChoice();
+    comScore.innerText = "Computer Score: " + computerScore;
+    yourScore.innerText = "Your Score: " + humanScore;
+    if (computerChoice === "Rock" && span.textContent === "You picked: Rock") {
+        resultSpan.innerText = "Tie";
+    } else if (computerChoice === "Paper" && span.textContent === "You picked: Paper") {
+        resultSpan.innerText = "Tie";
+    } else if (computerChoice === "Scissors" && span.textContent === "You picked: Scissors") {
+        resultSpan.innerText = "Tie";
+    } else if (computerChoice === "Rock" && span.textContent === "You picked: Scissors") {
+        resultSpan.innerText = " Computer won, Rock crushes Scissors";
+        computerScore++;
+        comScore.innerText = "Computer Score: " + computerScore;
+    } else if(computerChoice === "Paper" && span.textContent === "You picked: Scissors") {
+        resultSpan.innerText = "You won, Scissors cuts Paper";
+        humanScore++;
+        yourScore.innerText = "Your Score: " + humanScore;
+    } else if (computerChoice === "Scissors" && span.textContent === "You picked: Paper") {
+        resultSpan.innerText = " Computer won, Scissors cuts Paper";
+        computerScore++;
+        comScore.innerText = "Computer Score: " + computerScore;
+    } else if (computerChoice === "Rock" && span.textContent === "You picked: Paper") {
+        resultSpan.innerText = "You won, Paper covers Rock";
+        humanScore++;
+        yourScore.innerText = "Your Score: " + humanScore;
+    } else if (computerChoice === "Paper" && span.textContent === "You picked: Rock") {
+        resultSpan.innerText = " Computer won, Paper covers Rock";
+        computerScore++;
+        comScore.innerText = "Computer Score: " + computerScore;
+    } else if (computerChoice === "Scissors" && span.textContent === "You picked: Rock") {
+        resultSpan.innerText = "You won, Rock crushes Scissors";
+        humanScore++;
+        yourScore.innerText = "Your Score: " + humanScore;
     } else {
-      alert("You Won");
+        return;
     };
-    
 };
 
-playGame();
+function remainingMoves() {
+    if (clickCounter === 0 && computerScore < humanScore) {
+        scoreCard.textContent = "After Five Rounds, You won the game: " + humanScore + " - " + computerScore;
+        resetButton.textContent = "RESTART GAME";
+        containerTwo.appendChild(resetButton);
+        resetButton.disabled = false;
+        scissorsButton.disabled = true;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        return;
+    } else if (clickCounter === 0 && computerScore > humanScore) {
+        scoreCard.textContent = "After Five Rounds, Computer won the game: " + computerScore + " - " + humanScore;
+        resetButton.textContent = "RESTART GAME";
+        containerTwo.appendChild(resetButton);
+        resetButton.disabled = false;
+        scissorsButton.disabled = true;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        return;
+    } else if(clickCounter === 0 && computerScore === humanScore) {
+        scoreCard.textContent = "After Five Rounds, There was a Tie, Please play another round";
+        resetButton.textContent = "RESTART GAME";
+        containerTwo.appendChild(resetButton);
+        resetButton.disabled = false;
+        scissorsButton.disabled = true;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        return;
+    };
+};
+
+function reset() {
+    span.innerText = "";
+    cSpan.innerText = "";
+    resultSpan.innerText = "";
+    yourScore.innerText = "";
+    comScore.innerText = "";
+    scoreCard.innerText = "";
+    humanScore = 0;
+    computerScore = 0;
+    clickCounter = 4;
+    containerTwo.removeChild(resetButton);
+    scissorsButton.disabled = false;
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+};
+resetButton.addEventListener("click", reset);
--- rps.js
+++ rps.js
@@ -1,41 +1,150 @@
-const humanButton = document.querySelector("#button1");
-            const computerButton = document.querySelector("#button2");
-            const scoreButton = document.querySelector("#button3");
-            const container = document.querySelector("#container");
-            const form = document.querySelector("form");
-            const input = document.querySelector("input");
-            let span = document.querySelector("#input-text");
-            let cSpan = document.querySelector("#computer-text");
+const rockButton = document.querySelector("#button1");
+const paperButton = document.querySelector("#button2");
+const scissorsButton = document.querySelector("#button3");
+const container = document.querySelector("#container");
+const containerTwo = document.querySelector(".container-two")
+let span = document.querySelector("#input-text");
+let cSpan = document.querySelector("#computer-text");
+let resultSpan = document.querySelector("#round-winner");
+let yourScore = document.querySelector("#yourScore");
+let comScore = document.querySelector("#comScore");
+let scoreCard = document.querySelector("#scoreCard");
+let humanScore = 0;
+let computerScore = 0;
+let clickCounter = 4;
+let resetButton = document.createElement("button");
 
-            humanButton.addEventListener("click", humanBtn);
+function getComputerChoice() {
+const randomChoice = Math.ceil(Math.random() * 3);
+let computerPick = "";
+if (randomChoice === 1) {
+    computerPick = "Rock";
+    cSpan.textContent = "Computer Picked: " + computerPick;
+    return computerPick;
+} else if (randomChoice === 2) {
+    computerPick = "Paper";
+    cSpan.textContent = "Computer Picked: " + computerPick;
+    return computerPick;
+} else {
+    computerPick = "Scissors";
+    cSpan.textContent = "Computer Picked: " + computerPick;
+    return computerPick;
+};
+};
 
-            function humanBtn(e) {
-                e.preventDefault();
-                let userInput = input.value
-                form.reset();
-                input.focus();
-                span.textContent = "You Picked:           " + userInput;
-            };
+rockButton.addEventListener("click", chooseRock);
+function chooseRock() {
+    span.textContent = "You picked: Rock";
+    playGame();
+    remainingMoves();
+    comScore.textContent = "Computer Score: " + computerScore;
+    yourScore.textContent = "Your Score: " + humanScore;
+    clickCounter --;
+};
 
-            computerButton.addEventListener("click", getComputerChoice);
+paperButton.addEventListener("click", choosePaper);
+function choosePaper() {
+    span.textContent = "You picked: Paper";
+    playGame();
+    remainingMoves();
+    comScore.textContent = "Computer Score: " + computerScore;
+    yourScore.textContent = "Your Score: " + humanScore;
+    clickCounter --;
+};
 
-            function getComputerChoice() {
-                const randomChoice = Math.ceil(Math.random() * 3);
-                let computerPick = "";
+scissorsButton.addEventListener("click", chooseScissors);
+function chooseScissors() {
+    span.textContent = "You picked: Scissors";
+    playGame();
+    remainingMoves();
+    comScore.textContent = "Computer Score: " + computerScore;
+    yourScore.textContent = "Your Score: " + humanScore;
+    clickCounter --;
+};
 
-                if (randomChoice === 1) {
-                    computerPick = "rock";
-                    cSpan.textContent = "Computer Picked: " + computerPick;
-                    return computerPick;
+function playGame() {
+    let computerChoice = getComputerChoice();
+    comScore.innerText = "Computer Score: " + computerScore;
+    yourScore.innerText = "Your Score: " + humanScore;
+    if (computerChoice === "Rock" && span.textContent === "You picked: Rock") {
+        resultSpan.innerText = "Tie";
+    } else if (computerChoice === "Paper" && span.textContent === "You picked: Paper") {
+        resultSpan.innerText = "Tie";
+    } else if (computerChoice === "Scissors" && span.textContent === "You picked: Scissors") {
+        resultSpan.innerText = "Tie";
+    } else if (computerChoice === "Rock" && span.textContent === "You picked: Scissors") {
+        resultSpan.innerText = " Computer won, Rock crushes Scissors";
+        computerScore++;
+        comScore.innerText = "Computer Score: " + computerScore;
+    } else if(computerChoice === "Paper" && span.textContent === "You picked: Scissors") {
+        resultSpan.innerText = "You won, Scissors cuts Paper";
+        humanScore++;
+        yourScore.innerText = "Your Score: " + humanScore;
+    } else if (computerChoice === "Scissors" && span.textContent === "You picked: Paper") {
+        resultSpan.innerText = " Computer won, Scissors cuts Paper";
+        computerScore++;
+        comScore.innerText = "Computer Score: " + computerScore;
+    } else if (computerChoice === "Rock" && span.textContent === "You picked: Paper") {
+        resultSpan.innerText = "You won, Paper covers Rock";
+        humanScore++;
+        yourScore.innerText = "Your Score: " + humanScore;
+    } else if (computerChoice === "Paper" && span.textContent === "You picked: Rock") {
+        resultSpan.innerText = " Computer won, Paper covers Rock";
+        computerScore++;
+        comScore.innerText = "Computer Score: " + computerScore;
+    } else if (computerChoice === "Scissors" && span.textContent === "You picked: Rock") {
+        resultSpan.innerText = "You won, Rock crushes Scissors";
+        humanScore++;
+        yourScore.innerText = "Your Score: " + humanScore;
+    } else {
+        return;
+    };
+};
 
-                } else if (randomChoice === 2) {
-                    computerPick = "paper";
-                    cSpan.textContent = "Computer Picked: " + computerPick;
-                    return computerPick;
+function remainingMoves() {
+    if (clickCounter === 0 && computerScore < humanScore) {
+        scoreCard.textContent = "After Five Rounds, You won the game: " + humanScore + " - " + computerScore;
+        resetButton.textContent = "RESTART GAME";
+        containerTwo.appendChild(resetButton);
+        resetButton.disabled = false;
+        scissorsButton.disabled = true;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        return;
+    } else if (clickCounter === 0 && computerScore > humanScore) {
+        scoreCard.textContent = "After Five Rounds, Computer won the game: " + computerScore + " - " + humanScore;
+        resetButton.textContent = "RESTART GAME";
+        containerTwo.appendChild(resetButton);
+        resetButton.disabled = false;
+        scissorsButton.disabled = true;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        return;
+    } else if(clickCounter === 0 && computerScore === humanScore) {
+        scoreCard.textContent = "After Five Rounds, There was a Tie, Please play another round";
+        resetButton.textContent = "RESTART GAME";
+        containerTwo.appendChild(resetButton);
+        resetButton.disabled = false;
+        scissorsButton.disabled = true;
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        return;
+    };
+};
 
-                } else {
-                    computerPick = "scissors";
-                    cSpan.textContent = "Computer Picked: " + computerPick;
-                    return computerPick;
-                };
-            };
+function reset() {
+    span.innerText = "";
+    cSpan.innerText = "";
+    resultSpan.innerText = "";
+    yourScore.innerText = "";
+    comScore.innerText = "";
+    scoreCard.innerText = "";
+    humanScore = 0;
+    computerScore = 0;
+    clickCounter = 4;
+    containerTwo.removeChild(resetButton);
+    scissorsButton.disabled = false;
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+};
+resetButton.addEventListener("click", reset);
